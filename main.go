5928df8646cc80fa59c93b1dc28637b4b419e166
Ryan <ryannitz@amazon.com>
2018-11-14 11:32:47 -0500

    Initial

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"fmt"
+	"io/ioutil"
+
+	_ "github.com/aws/aws-xray-sdk-go/plugins/ecs"
+	"github.com/aws/aws-xray-sdk-go/xray"
+)
+
+const appName = "eks-workshop-x-ray-sample-front"
+
+func init() {
+	xray.Configure(xray.Config{
+		DaemonAddr:     "xray-service.default:2000",
+		LogLevel:       "info",
+		ServiceVersion: "1.2.3",
+	})
+}
+
+func main() {
+	http.Handle("/", xray.Handler(xray.NewFixedSegmentNamer(appName), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		resp, err := http.Get("http://x-ray-sample-back-k8s.default:8080")
+		defer resp.Body.Close()
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(resp.Status)
+
+		if resp.StatusCode == http.StatusOK {
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			io.WriteString(w, string(body))
+		}
+	})))
+	http.ListenAndServe(":8080", nil)
+}
+
